28a5c368e11e2c63afa903d204b1b3eab83cb9ba
DavidVelaSalvador <90475068+DavidVelaSalvador@users.noreply.github.com>
2021-09-10 20:48:24 +0200

    Initial commit

---
+++ Prueba_WPF/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prueba_WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private ObjLista Listado = new ObjLista();
+        private void Main_Initialized(object sender, EventArgs e)
+        {
+            this.Show();
+            Listado.RecibirDatos(this);
+            MostrarDatos();
+        }
+        private void MostrarDatos()// carga la lista del formulario toda la lista que hemos recopilado.
+        {
+            int i = 0;
+            Lista.Items.Clear();
+            for (i = 0; i < (Listado.Count() - 1); i++)
+            {
+                Lista.Items.Add(Listado.Nombre(i));
+                this.Title = "Añadido personaje:" + Listado.Nombre(i);
+            }
+        }
+        private bool HeightOk(int Altura)
+        {
+            if (Altura > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private bool DataOk(string cadena)// devuelve verdadero solo si la cadena no es alguno de estos casos
+        {
+            switch (cadena.Trim().ToUpper())
+            {
+                case "N/A": return false;
+                case "NONE": return false;
+                case "": return false;
+                case "NINGUNO": return false;
+                case "NINGUNA": return false;
+                case "VACIO": return false;
+                case "DESCONOCIDO": return false;
+                case "UNKNOWN": return false;
+                default: return true;
+            }
+        }
+        private void Lista_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LblNombre.Content = "Nombre: " + Listado.Nombre(Lista.SelectedIndex);
+            if (DataOk(Listado.Nombre(Lista.SelectedIndex)) == true)
+            {
+                LblNombre.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                LblNombre.Visibility = System.Windows.Visibility.Hidden;
+            }
+            LblAltura.Content = "Altura: " + Listado.Altura(Lista.SelectedIndex);
+            if (HeightOk(Listado.Altura(Lista.SelectedIndex)) == true)
+            {
+                LblAltura.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                LblAltura.Visibility = System.Windows.Visibility.Hidden;
+            }
+            LblColorPelo.Content = "Color de pelo: " + Listado.ColorPelo(Lista.SelectedIndex);
+            if (DataOk(Listado.ColorPelo(Lista.SelectedIndex)) == true)
+            {
+                LblColorPelo.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                LblColorPelo.Visibility = System.Windows.Visibility.Hidden;
+            }
+            LblGenero.Content = "Género: " + Listado.Genero(Lista.SelectedIndex);
+            if (DataOk(Listado.Genero(Lista.SelectedIndex)) == true)
+            {
+                LblGenero.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                LblGenero.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void BtnSortByName_Click(object sender, RoutedEventArgs e)
+        {
+            Listado.SortByName();
+            MostrarDatos();
+        }
+
+        private void BtnSoprtByHeight_Click(object sender, RoutedEventArgs e)
+        {
+            Listado.SortByHeight();
+            MostrarDatos();
+        }
+    }
+}
---
+++ Prueba_WPF/ObjLista.cs
@@ -0,0 +1,117 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_WPF
+{
+    class ObjLista
+    {
+        private MainWindow Formulario;
+        public class ObjPersonaje
+        {
+            public string name { get; set; }
+            public int height { get; set; }
+            public string mass { get; set; }
+            public string hair_color { get; set; }
+            public string skin_color { get; set; }
+            public string eye_color { get; set; }
+            public string birth_year { get; set; }
+            public string gender { get; set; }
+            public string homeworld { get; set; }
+        }
+
+        List<ObjPersonaje> Lista = new List<ObjPersonaje>();
+        WebClient client = new WebClient();
+        private String DatosRecibidos = "";
+        public bool DatosPersonaje(int index)// pide datos a la API y devuelve verdadero si hemos obtenido respuesta, falso en caso contrario.
+        {
+            try
+            {
+                DatosRecibidos = client.DownloadString("https://swapi.dev/api/people/" + index);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+        public void RecibirDatos(MainWindow form_aux) 
+        {
+            Formulario = form_aux;
+            int i = 0;
+            for (i = 0; i < 100; i++)// Solo hay 82 personajes dados de alta en la base de datos.
+            {
+                if (DatosPersonaje(i))
+                {
+                    Add();
+                }
+            }
+        }
+        private void Add()// añadimos a la lista un nuevo elemento con los datos recibidos.
+        {
+            ObjPersonaje Obj = new ObjPersonaje { };
+            try
+            {
+                Obj = JsonConvert.DeserializeObject<ObjPersonaje>(DatosRecibidos);
+                Lista.Add(Obj);
+                Formulario.Title = "Cargando_datos..." + Lista.Count() + "  (Este proceso puede ralentizarse dependiendo de la web swapi)";
+                Formulario.Progreso.Value = Lista.Count();
+                Formulario.Grid.UpdateLayout();
+            }
+            catch
+            {
+                Formulario.Title = "Fallo al insertar el personaje:" + Lista.Count;
+            }
+
+        }
+        public void SortByName()
+        {
+            Lista = Lista.OrderBy(ObjPersonaje => ObjPersonaje.name).ToList();
+        }
+        public void SortByHeight()
+        {
+            Lista = Lista.OrderBy(ObjPersonaje => ObjPersonaje.height).ToList();
+        }
+        public int Count()
+        {
+            return Lista.Count;
+        }
+        public string Nombre(int index)
+        {
+            if (index < 0)
+            {
+                index = 0;
+            }
+            return Lista[index].name;
+        }
+        public int Altura(int index)
+        {
+            if (index < 0)
+            {
+                index = 0;
+            }
+            return Lista[index].height;
+        }
+        public string ColorPelo(int index)
+        {
+            if (index < 0)
+            {
+                index = 0;
+            }
+            return Lista[index].hair_color;
+        }
+        public string Genero(int index)
+        {
+            if (index < 0)
+            {
+                index = 0;
+            }
+            return Lista[index].gender;
+        }
+    }
+}
